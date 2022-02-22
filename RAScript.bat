8ad1f4fa1102069a3b1a7ec0f6a298713a11b3e5
Gamr13 <48205338+Gamr13@users.noreply.github.com>
2022-02-22 11:52:26 +0000

    Updated to v1.01
    - Fixed an issue with extracting the old_cores.7z file (Missed a ") 
    - Removed some working cores from the "Deleting broken RetroArch cores..." list

--- RAScript.bat
+++ RAScript.bat
@@ -1,6 +1,9 @@
 echo "Git cloning RetroArch & old cores..."
 git clone --recurse-submodules -j8 https://github.com/libretro/RetroArch.git
 git clone https://github.com/Gamr13/old_cores.git
+move /Y ".\old_cores\ADLLS.7z" "..\RA" 
+move /Y ".\old_cores\ADLLS2.7z" "..\RA"
+move /Y ".\old_cores\old_cores.7z" "..\RA"
 
 echo "Downloading RetroArch cores..."
 curl "https://buildbot.libretro.com/nightly/windows/x86_64/RetroArch_cores.7z" --Output cores.7z
@@ -10,15 +13,16 @@ curl "http://195.90.212.190/Files/Angle%%20Cores/yabasanshiro_libretro.dll" --Ou
 
 echo "Extracting Files..."
 "C:\Program Files\7-Zip\7z.exe" x cores.7z -aoa -o".\cores"
-"C:\Program Files\7-Zip\7z.exe" x ".\old_cores\old_cores.7z -aoa -o".\RetroArch\pkg\msvc-uwp\RetroArch-msvc2019-UWP\cores\x64\cores\"
-"C:\Program Files\7-Zip\7z.exe" x ".\old_cores\ADLLS.7z" -aoa -o".\ADLLS"
-"C:\Program Files\7-Zip\7z.exe" x ".\old_cores\ADLLS2.7z" -aoa -o".\ADLLS"
+"C:\Program Files\7-Zip\7z.exe" x "old_cores.7z" -aoa -o".\old_cores"
+"C:\Program Files\7-Zip\7z.exe" x "ADLLS.7z" -aoa -o".\ADLLS"
+"C:\Program Files\7-Zip\7z.exe" x "ADLLS2.7z" -aoa -o".\ADLLS"
 
 echo "Moving core dependencies..."
 move /Y ".\ADLLS\*" "RetroArch\pkg\msvc-uwp\RetroArch-msvc2019-UWP\cores\x64\"
 
 echo "Moving core files..."
 move /Y ".\cores\RetroArch-Win64\cores\*" "RetroArch\pkg\msvc-uwp\RetroArch-msvc2019-UWP\cores\x64\cores\"
+move /Y ".\old_cores\*.dll" "RetroArch\pkg\msvc-uwp\RetroArch-msvc2019-UWP\cores\x64\cores\"
 
 echo "Deleting non-ANGLE core files..."
 del "RetroArch\pkg\msvc-uwp\RetroArch-msvc2019-UWP\cores\x64\cores\mupen64plus_next_libretro.dll"
@@ -32,12 +36,11 @@ move /Y "yabasanshiro_libretro.dll" "RetroArch\pkg\msvc-uwp\RetroArch-msvc2019-U
 
 echo "Cleaning up files..."
 del *.7z *.dll 
-echo y|rmdir /s ".\cores" ".\old_cores"
-echo y|rmdir /s ".\old_cores"
-echo y|rmdir /s ".\ADLLS"
+echo Y|rmdir /s ".\cores" ".\old_cores"
+echo Y|rmdir /s ".\old_cores"
+echo Y|rmdir /s ".\ADLLS"
 
 echo "Deleting broken RetroArch cores..."
 cd "RetroArch\pkg\msvc-uwp\RetroArch-msvc2019-UWP\cores\x64\cores\"
-del .empty 81_*.dll bk_*.dll chailove*.dll craft*.dll easyrpg*.dll fuse*.dll mednafen_psx_hw*.dll fbalpha*.dll mednafen_snes*.dll mednafen_gba*.dll hatari*.dll x1*.dll bsnes_mer*.dll bsnes2*.dll mesen-s*.dll minivm*.dll 2048*.dll arduous*.dll blastem*.dll boom3*.dll boom3*.dll cannonball_*.dll citra*.dll crocods*.dll dinothawr_*.dll ecwolf*.dll frodo*.dll gong*.dll jaxe*.dll jumpnbump*.dll lowresnx*.dll lutro*.dll mrboom*.dll mu*.dll nekop2*.dll nxengine*.dll oberon*.dll play_*.dll prboom*.dll race*.dll reminiscence*.dll retro8*.dll sameduck*.dll snes9x2*.dll squirreljme*.dll stella2*.dll tgbdual*.dll thepowdertoy*.dll tic80*.dll tyrquake*.dll uzem*.dll vitaquake2*.dll vemulator*.dll wasm4*.dll xrick*.dll                    							
+del .empty bk_*.dll craft*.dll easyrpg*.dll mednafen_psx_hw*.dll mednafen_snes*.dll mednafen_gba*.dll bsnes_mer*.dll bsnes2*.dll mesen-s*.dll minivm*.dll arduous*.dll boom3*.dll boom3*.dll citra*.dll crocods*.dll frodo*.dll gong*.dll jaxe*.dll jumpnbump*.dll mrboom*.dll mu*.dll nekop2*.dll nxengine*.dll oberon*.dll play_*.dll race*.dll sameduck*.dll snes9x2*.dll squirreljme*.dll stella2*.dll tyrquake*.dll vitaquake2*.dll wasm4*.dll                    							
 pause 
-
